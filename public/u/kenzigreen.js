5c42936c191921b4bbd41d8fd751504e17619154
BraNFT™ <99556664+branfts@users.noreply.github.com>
2024-07-27 14:41:46 -0700

    Update kenzigreen.js

--- public/u/kenzigreen.js
+++ public/u/kenzigreen.js
@@ -1,7 +1,8 @@
 
 const links = [
     {
-        "url": "https://kenzigreendesign.com/"
+        "url": "https://kenzigreendesign.com/",
+        favicon: 'https://static.showit.co/200/z35PaIDmTp6DmVEY8qB8Sg/95346/brown_sparkles-41.png'
     },
     {
         "url": "https://www.pinterest.com/KenziGreenDesign/"
