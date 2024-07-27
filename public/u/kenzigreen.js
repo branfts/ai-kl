fe5b31e6c7cb680fc766b8457b1b1fd5b3e0bbec
branfts <support@june07.com>
2024-07-27 14:34:25 -0700

    Updated by A10x Service

---
+++ public/u/kenzigreen.js
@@ -0,0 +1,23 @@
+
+const links = [
+    {
+        "url": "https://kenzigreendesign.com/"
+    },
+    {
+        "url": "https://www.pinterest.com/KenziGreenDesign/"
+    },
+    {
+        "url": "https://www.facebook.com/KenziGreenDesign"
+    },
+    {
+        "url": "https://www.instagram.com/kenzigreendesign"
+    },
+    {
+        "url": "https://www.tiktok.com/@kenzigreendesign"
+    },
+    {
+        "url": "https://www.youtube.com/channel/UCwHqV67gkJD8MSfI_OpnXlQ/featured"
+    }
+];
+const rules = [];
+export { links, rules };
