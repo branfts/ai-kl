6631c2c5d6912d2bc39e43e3d30ec1b5a712d4db
BraNFT™ <99556664+branfts@users.noreply.github.com>
2024-07-28 12:51:29 -0700

    .

--- public/u/katliente.js
+++ public/u/katliente.js
@@ -1,21 +1,26 @@
 
 const links = [
     { url: 'http://discord.gg/katliente' },
-{ url: 'https://www.katliente.com/free-assets' },
-{ url: 'https://ko-fi.com/katliente' },
-{ url: 'https://www.etsy.com/shop/KatlienteTwitch?ref=simple-shop-header-name&listing_id=1051208417' },
-{ url: 'https://www.amazon.com/shop/influencer-322e9065' },
-{ url: 'https://twitch.tv/katliente' },
-{ url: 'https://www.youtube.com/@katliente' },
-{ url: 'https://twitter.com/katlienteee' },
-{ url: 'https://instagram.com/katliente.ttv' },
-{ url: 'https://tiktok.com/@katliente' },
-{ url: 'https://bit.ly/3W8OkNq' },
-{ url: 'https://creators.riverside.fm/Katliente' },
-{ url: 'https://beacons.ai/katliente' }
+    { url: 'https://www.katliente.com' },
+    { url: 'https://ko-fi.com/katliente' },
+    { url: 'https://www.etsy.com/shop/KatlienteTwitch?ref=simple-shop-header-name&listing_id=1051208417' },
+    { url: 'https://www.amazon.com/shop/influencer-322e9065' },
+    { url: 'https://twitch.tv/katliente' },
+    { url: 'https://www.youtube.com/@katliente' },
+    { url: 'https://twitter.com/katlienteee' },
+    { url: 'https://instagram.com/katliente.ttv' },
+    { url: 'https://tiktok.com/@katliente' },
+    { url: 'https://bit.ly/3W8OkNq' },
+    { url: 'https://creators.riverside.fm/Katliente', favicon: 'https://cdn.prod.website-files.com/5f996b22b00afe35a55e6f79/62b46d53dc8fb83b969ab878_RS_favicon-black.png' },
+    { url: 'https://beacons.ai/katliente' }
 ]
 const rules = [
-    
+    {
+        priority: 0,
+        name: 'redirect',
+        url: 'https://twitch.tv/katliente',
+        timeout: 10
+    }
 ]
 
 export { links, rules }
