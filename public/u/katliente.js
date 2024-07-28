e83d9931091bdb9193d145921d49aeea626075bc
branfts <support@june07.com>
2024-07-28 12:33:06 -0700

    Updated by A10x Service

---
+++ public/u/katliente.js
@@ -0,0 +1,21 @@
+
+const links = [
+    { url: 'http://discord.gg/katliente' },
+{ url: 'https://www.katliente.com/free-assets' },
+{ url: 'https://ko-fi.com/katliente' },
+{ url: 'https://www.etsy.com/shop/KatlienteTwitch?ref=simple-shop-header-name&listing_id=1051208417' },
+{ url: 'https://www.amazon.com/shop/influencer-322e9065' },
+{ url: 'https://twitch.tv/katliente' },
+{ url: 'https://www.youtube.com/@katliente' },
+{ url: 'https://twitter.com/katlienteee' },
+{ url: 'https://instagram.com/katliente.ttv' },
+{ url: 'https://tiktok.com/@katliente' },
+{ url: 'https://bit.ly/3W8OkNq' },
+{ url: 'https://creators.riverside.fm/Katliente' },
+{ url: 'https://beacons.ai/katliente' }
+]
+const rules = [
+    
+]
+
+export { links, rules }
